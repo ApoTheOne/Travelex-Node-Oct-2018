0bf15b3a29e204e0dc1cf1d4f15a46a1fe9030fb
Magesh <tkmagesh77@gmail.com>
2018-10-25 12:19:50 +0530

    refactoring complete

--- refactored/serveStatic.js
+++ refactored/serveStatic.js
@@ -7,15 +7,23 @@ function isStatic(resource){
 	return staticExtns.indexOf(path.extname(resource)) >= 0 ;
 }
 
-module.exports = function(req, res){
-	if (isStatic(req.urlObj.pathname)){
-		var resource = path.join(__dirname, req.urlObj.pathname);
-		if (!fs.existsSync(resource)){
-			res.statusCode = 404;
-			res.end();
-			return;
+module.exports = function(staticFolderPath){
+	return function(req, res, next){
+		if (isStatic(req.urlObj.pathname)){
+			var resource = path.join(staticFolderPath, req.urlObj.pathname);
+			if (!fs.existsSync(resource)){
+				res.statusCode = 404;
+				res.end();
+				return;
+			}
+
+			var stream = fs.createReadStream(resource);
+			stream.pipe(res);
+			stream.on('end',function(){
+				next();
+			});
+		} else {
+			next();
 		}
-		var stream = fs.createReadStream(resource);
-		stream.pipe(res);
 	}
 }
